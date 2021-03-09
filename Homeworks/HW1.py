1490932af860d93582ceb10eeb5e0c0fb75e7025
Mehmet Barış POLAT <51261730+mehmetbarispolat@users.noreply.github.com>
2021-03-09 21:27:30 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,11 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+first_list = list(range(0,10,2))
+second_list = list(range(1,10,2))
+
+new_list = first_list + second_list
+new_list = [x*2 for x in new_list]
+
+for i in new_list:
+    print(type(i))
