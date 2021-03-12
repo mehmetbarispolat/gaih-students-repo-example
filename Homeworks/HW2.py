e978e0ab38044c1a9df394fa9c89bb32ed642346
Mehmet Barış POLAT <51261730+mehmetbarispolat@users.noreply.github.com>
2021-03-12 18:31:49 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,32 @@
-#Explain your work
+cvs = {
+    'cv_1': {
+        'Name' : "Ahmet",
+        'Surname' : "Poyraz",
+        'Job Title' : "Software Developer"
+    },
+    'cv_2' : {
+        'Name' : "Veli",
+        'Surname' : "Poyraz",
+        'Job Title' : "Backend Developer"
+    }, 
+    'cv_3' : {
+        'Name' : "Ali",
+        'Surname' : "Polat",
+        'Job Title' : "Frontend Developer"
+    }, 
+    'cv_4' : {
+        'Name' : "Cengiz",
+        'Surname' : "Deniz",
+        'Job Title' : "Software Engineer"
+    }, 
+    'cv_5' : {
+        'Name' : "Ömer",
+        'Surname' : "Seyfettin",
+        'Job Title' : "Writer"
+    }
+} 
 
-#Question 1
-for x in range(a):
-	print(a)
+for key, value in cvs.items():
+    print(f'{key} -- ')
+    for k,v in value.items():
+        print(f'{k} : {v}')
