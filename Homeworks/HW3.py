597d6cd4a708d307ae41bfa4dbe38af72c441cf2
Mehmet Barış POLAT <51261730+mehmetbarispolat@users.noreply.github.com>
2021-03-14 02:55:35 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,24 @@
-#Explain your work
+students = dict()
+list_of_grade = []
+def course_grade():
+    for index in range(5):
+        students[index] = {}
+        mid_term = int(input("Enter Mid Term Grade: "))
+        project = int(input("Enter Project Grade: "))
+        final = int(input("Enter Final Grade: "))
+        passing_grade(index,mid_term,project,final)
+        students[index]["mid_term"] = mid_term
+        students[index]["project"] = project
+        students[index]["final"] = final
+    sorting()
 
-#Question 1
-for x in range(a):
-	print(a)
+def passing_grade(index,mid_term,project,final):
+    passing_grade = mid_term * 0.3 + project * 0.3 + final * 0.4
+    list_of_grade.insert(index,passing_grade)
+
+def sorting():
+    list_of_grade.sort(reverse = True)
+
+if __name__ == '__main__':
+    course_grade()
+    print(list_of_grade)
