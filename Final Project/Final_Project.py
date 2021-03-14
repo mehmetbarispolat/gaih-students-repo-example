e44ebb574efed170e2521970169f55332cd2fffc
Mehmet Barış POLAT <51261730+mehmetbarispolat@users.noreply.github.com>
2021-03-14 22:00:32 +0300

    Update Final_Project.py

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -1,3 +1,23 @@
-import pandas as pd
+questions = ["Türkiye'nin başkenti neresidir?",
+"Japonların geleneksel güreşine ne ad verilir?",
+"Deniz yolculuğunda toplu insan taşımacılığında kullanılan araçların ortak adı?",
+"Kurutulmuş yaprakları demlendirilerek içilen bitki?","Ölenden geriye kalan mal veya para",
+"Ağırlık ölçüsü içinde bin kilogramlık bir yük kaç ton gelir?",
+"Şirketlere ait hisseler nerede alınır satılır?",
+"TC kimlik numaramız kaç harften oluşur?",
+"Yeni bir işe girerken, iş yerlerine verdiğimiz kısa öz geçmişin kısa adı",
+"Lakin ve fakatın eş anlamlısı"]
+answers = ["Ankara","Sumo","Vapur","Çay","Miras","1 ton",'Borsa','11','CV','Ama']
+LEN_ARRAY = len(questions)
+count = 0
+for i in range(LEN_ARRAY):
+    print(questions[i])
+    user_answer = input("Answer --> ")
+    if user_answer == answers[i]:
+        count += 1
 
-data = pd.read_csv("project.csv")
+worth = count * 10
+if count > 5:
+    print(f'success {worth} points')
+else:
+    print(f'fail {worth} points')
